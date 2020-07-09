7de7ae25e34c12103be111b08934195a1c72e33b
frapimoneto <frapimoneto@gmail.com>
2020-07-08 22:57:06 -0300

    .

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,26 +1,28 @@
 import { Router } from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
 // import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
 
 transactionsRouter.get('/', async (request, response) => {
-  // TODO
+  
 });
 
 transactionsRouter.post('/', async (request, response) => {
-  // TODO
+  const { title, value, type, category } = request.body;
+
+  const createTransaction = new CreateTransactionService();
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
-  // TODO
+  
 });
 
 transactionsRouter.post('/import', async (request, response) => {
-  // TODO
+  
 });
 
 export default transactionsRouter;
